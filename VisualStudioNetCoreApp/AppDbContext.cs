ca8d8d4f77da71788f16233bce9358860910d3cc
Alper Silistre <alpersilistre@gmail.com>
2018-01-29 02:08:11 +0300

    Add project files.

---
+++ VisualStudioNetCoreApp/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VisualStudioNetCoreApp
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
